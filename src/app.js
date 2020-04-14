0b1ec6ca678398813b1e551219a7864e8053696a
Halee Pagel <halee.pagel@gmail.com>
2020-04-14 21:35:22 +0900

    I have an idea of how I'll visualize things. Now to get on with it.

--- src/app.js
+++ src/app.js
@@ -8,9 +8,11 @@ const Sort = require("./Sort");
 
 // A link to our styles!
 require("./index.css");
-
-const sort = new Sort();
-sort.sort();
+console.log(
+  "This is the typeof const Sort from app.js: " + new Sort([7, 8, 5, 3, 9, 0])
+);
+//const sort = new Sort();
+//sort.sort();
 
 function createCheesyTitle(slogan) {
   const container = document.createElement("h1");
@@ -19,11 +21,17 @@ function createCheesyTitle(slogan) {
   return container;
 }
 
-const title = createCheesyTitle(sort.returnValue("Re-Engineer Yourself"));
+const title = createCheesyTitle("What is a Heap Sort Algorithm?");
 document.getElementById("title").appendChild(title);
 
+// append BEFORE ARRAY [ 1, 3, 2 ] to the DOM with sort.sort
+
+// then append the AFTER ARRAY [ 1, 2, 3] to the DOM with sort.sort
+
+// put each array number in a div, then change the size/look of the div to match the number
+
 /*
-    An simple example of how you can make your project a bit more
+    A simple example of how you can make your project a bit more
     interactive, if you would like.
 
     In our `index.html` page, we have a short form.

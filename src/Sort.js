120d9814477f6d642c26f626467c4737b710922b
Halee Pagel <halee.pagel@gmail.com>
2020-04-14 21:34:17 +0900

    Algorithm sorted, pardon the pun.

--- src/Sort.js
+++ src/Sort.js
@@ -1,14 +1,95 @@
-class SomeKindOfSort {
+"use strict";
+class minHeapSort {
   constructor(array) {
+    if (!array) {
+      console.log(
+        "Please create a new instance of minHeapSort & pass an array as an argument."
+      );
+      return;
+    }
     this.array = array;
+    this.unsortedArraySize = array.length;
   }
+  // the sort method kicks things off and (pardon the pun) it sort of acts as ground control for the other methods
   sort() {
-    console.log("sort!");
-  }
+    console.log("Let's create our minHeapSort!");
+    // we'll get things started by creating the minHeap
+    this.createTheMinHeap();
 
-  returnValue(value) {
-    return value;
+    // the for loop below will switch index 0 and the end element
+    // we'll decrease this.arraySize because we know with each iteration
+    // the array becomes more sorted at the right side
+    for (let i = this.array.length - 1; i >= 0; i--) {
+      // arrays are passed by reference, create a new variable for this.array in order to access it inside the context of the switchPlaces function
+      const arr = this.array;
+      // I know writing switchPlaces twice in the code is anti-D.R.Y. but I couldn't figure out how to make it a method and still access this.array inside
+      function switchPlaces(x, y) {
+        // create a temp var to hold onto the value before we replace it
+        let temporaryValueHolder = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temporaryValueHolder;
+        return arr;
+      }
+      switchPlaces(0, i);
+      // now that we bubbled the largest number to the top and switched its place with the last element in the array
+      // let's decreased the size of this.unsortedArraySize because we know that the far right element is locked in place
+      // no need to bother checking it again
+      this.unsortedArraySize--;
+      // after we have decreased the size of this.unsortedArraySize we start the whole thing over again and bubble up the next highest value
+      this.minHeapComparisonCheck(0);
+    }
+    console.log("All finished! Here is the minHeapSort: " + this.array);
+    return this.array;
+  }
+  // the createTheMinHeap method finds the last group of parent/child elements and then runs minHeapComparisonCheck with that group
+  createTheMinHeap() {
+    // because of the work of brilliant mathematical minds, the formula for finding all of the parent elements is as follows:
+    for (let i = Math.floor(this.array.length / 2); i >= 0; i--) {
+      this.minHeapComparisonCheck(i);
+    }
+  }
+  // the minHeapComparisonCheck method will compare the parent and child elements
+  minHeapComparisonCheck(j) {
+    // once again, brilliant mathematical minds have proved that finding the leftChild index is the below formula:
+    // i diagrammed this out on my mini-whiteboard and it worked! Math is magic.
+    let leftChild = 2 * j + 1;
+    // and those same brilliant mathematical minds went on and proved how to find the rightChild index!
+    let rightChild = 2 * j + 2;
+
+    let indexOfLargestNumFromParentChildComparison = j;
+
+    if (
+      leftChild < this.unsortedArraySize &&
+      this.array[leftChild] > this.array[j]
+    ) {
+      indexOfLargestNumFromParentChildComparison = leftChild;
+    }
+    if (
+      rightChild < this.unsortedArraySize &&
+      this.array[rightChild] >
+        this.array[indexOfLargestNumFromParentChildComparison]
+    ) {
+      indexOfLargestNumFromParentChildComparison = rightChild;
+    }
+    // if indexOfLargestNumFromParentChildComparison is REASSIGNED
+    // that means we need to officially switch the indexes of the parent element and one of the children
+    if (indexOfLargestNumFromParentChildComparison != j) {
+      // arrays are passed by reference, create a new variable for this.array in order to access it inside the context of the switchPlaces function
+      const arr = this.array;
+      // I know writing switchPlaces twice in the code is anti-D.R.Y. but I couldn't figure out how to make it a method and still access this.array inside
+      function switchPlaces(x, y) {
+        // create a temp var to hold onto the value before we replace it
+        let temporaryValueHolder = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temporaryValueHolder;
+        return arr;
+      }
+      switchPlaces(j, indexOfLargestNumFromParentChildComparison);
+      // after the official switch has been made
+      // let's do this check once more to compare if the unswitched child is greater than this updated value at indexOfLargestNumFromParentChildComparison
+      this.minHeapComparisonCheck(indexOfLargestNumFromParentChildComparison);
+    }
   }
 }
 
-module.exports = SomeKindOfSort;
+module.exports = minHeapSort;
--- test/_sort.js
+++ test/_sort.js
@@ -5,7 +5,158 @@ describe("Sort", () => {
   it("should be a function", () => {
     expect(Sort).to.be.a("function");
   });
+
   it("should have a sort method", () => {
     expect(Sort.prototype.sort).to.be.a("function");
+  });
+
+  it("should have a createTheMinHeap method", () => {
+    expect(Sort.prototype.createTheMinHeap).to.be.a("function");
+  });
+
+  it("should have a minHeapComparisonCheck method", () => {
+    expect(Sort.prototype.minHeapComparisonCheck).to.be.a("function");
+  });
+
+  it("should create a new instance of minHeapSort", () => {
+    // setup
+    const aNewInstance = new Sort([7, 6, 89, 10, 11, -12, -13]);
+    // assert
+    expect(aNewInstance).to.be.an("object");
+  });
+
+  it("should check that an array is passed as an argument when a new instance of minHeapSort is created", () => {
+    // setup
+    const aNewInstance = new Sort([7, 6, 89, 10, 11, -12, -13]);
+    const passedInArray = [7, 6, 89, 10, 11, -12, -13];
+    // assert
+    expect(aNewInstance.array).to.deep.equal(passedInArray);
+  });
+
+  it("should request a new instance of the class be created if an array is not passed as an argument when a new instance of minHeapSort is created", () => {
+    // setup
+    const aNewInstance = new Sort();
+    // assert
+    expect(aNewInstance.array).to.deep.equal(undefined);
+  });
+
+  it("should return a minimum sorted array", () => {
+    // setup
+    const aNewInstance = new Sort([7, 6, 89, 10, 11, -12, -13]);
+    const minSortedArray = [-13, -12, 6, 7, 10, 11, 89];
+    // assert
+    expect(aNewInstance.sort()).to.deep.equal(minSortedArray);
+  });
+
+  it("should NOT return a maximum sorted array", () => {
+    // setup
+    const aNewInstance = new Sort([7, 6, 89, 10, 11, -12, -13]);
+    const maxSortedArray = [89, 11, 10, 7, 6, -12, -13];
+    // assert
+    expect(aNewInstance.sort()).to.not.deep.equal(maxSortedArray);
+  });
+
+  it("should return a minimum heap sorted array for a very small array", () => {
+    // setup
+    const aNewInstance = new Sort([2, 1]);
+    const minSortedArray = [1, 2];
+    // assert
+    expect(aNewInstance.sort()).to.deep.equal(minSortedArray);
+  });
+
+  it("should return a minimum heap sorted array for a very large array", () => {
+    // setup
+    const aNewInstance = new Sort([
+      2,
+      1,
+      8,
+      7,
+      3,
+      6,
+      1234567890,
+      -1234567890,
+      4,
+      9,
+      5,
+      87654,
+      10,
+      20,
+      -432,
+      -99999,
+      -21,
+      11,
+      13,
+      15,
+      12,
+      16,
+      17,
+      14,
+      18,
+      -567,
+      832,
+      -91,
+      19,
+      71,
+      90,
+      44,
+      63,
+      72,
+      59,
+      445,
+      31,
+      -8,
+      -21,
+    ]);
+    const minSortedArray = [
+      -1234567890,
+      -99999,
+      -567,
+      -432,
+      -91,
+      -21,
+      -21,
+      -8,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      31,
+      44,
+      59,
+      63,
+      71,
+      72,
+      90,
+      445,
+      832,
+      87654,
+      1234567890,
+    ];
+    // assert
+    expect(aNewInstance.sort()).to.deep.equal(minSortedArray);
+  });
+
+  it("should account for duplicate numbers by placing them next to each other", () => {
+    // setup
+    const aNewInstance = new Sort([2, 3, 1, 8, 2, 1, 8, 3]);
+    const minSortedWithDupesArray = [1, 1, 2, 2, 3, 3, 8, 8];
+    // assert
+    expect(aNewInstance.sort()).to.deep.equal(minSortedWithDupesArray);
   });
 });
